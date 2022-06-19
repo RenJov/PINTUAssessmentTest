cb4bd743aa141ceae1cd9654a0d156c7cc8466a5
Muhammad Renaldy Jovananda <muhammad.jovananda@dana.id>
2022-06-19 11:48:22 +0700

    initial commit

---
+++ src/test/java/helper/ApiHelper.java
@@ -0,0 +1,65 @@
+package helper;
+
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.testng.Assert;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import static io.restassured.RestAssured.given;
+
+public class ApiHelper {
+
+    public Response getApiTest(String endpoint, String requestBody, Integer expectedHttpStatusCode) {
+        Response responseGetApi = given()
+                .when()
+                .body(requestBody)
+                .get(endpoint);
+
+        Assert.assertEquals(responseGetApi.statusCode(),expectedHttpStatusCode);
+        return responseGetApi;
+    }
+
+    public Response postApiTest(String endpoint, String requestBody, Integer expectedHttpStatusCode) {
+        Response responsePostApi = given()
+                .when()
+                .header("Content-Type", "application/json")
+                .body(requestBody)
+                .post(endpoint);
+
+        Assert.assertEquals(responsePostApi.statusCode(),expectedHttpStatusCode);
+        return responsePostApi;
+    }
+
+    public String getJsonStringFromFile (String filePath) throws Exception {
+        String jsonString = null;
+        JSONParser jsonParser = new JSONParser();
+
+        try (FileReader reader = new FileReader(filePath)) {
+            Object obj = jsonParser.parse(reader);
+            jsonString = obj.toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return jsonString;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String jsonString = null;
+        JSONParser jsonParser = new JSONParser();
+
+        try (FileReader reader = new FileReader("src/main/resources/postRequest.json")) {
+            Object obj = jsonParser.parse(reader);
+            jsonString = obj.toString();
+            System.out.println("json: "+jsonString);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/test/java/testSuite/ApiTest.java
@@ -0,0 +1,74 @@
+package testSuite;
+
+import helper.ApiHelper;
+import io.restassured.response.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class ApiTest {
+
+    ApiHelper apiHelper = new ApiHelper();
+
+    private final static String ENDPOINT = "https://jsonplaceholder.typicode.com/posts";
+    private final static String JSON_POST_REQUEST = "src/main/resources/postRequest.json";
+
+    @Test
+    public void getTest() {
+
+        Response response = apiHelper.getApiTest(ENDPOINT, "", 200);
+        System.out.println("GET Response: "+response.getBody().asString());
+
+        JSONArray jsonArray = new JSONArray(response.getBody().asString());
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            assertDataType(null, jsonObject);
+        }
+    }
+
+    @Test
+    public void postTest() throws Exception {
+
+        String requestJsonString = apiHelper.getJsonStringFromFile(JSON_POST_REQUEST);
+        JSONObject requestJson = new JSONObject(requestJsonString);
+        System.out.println("POST Request: "+requestJson.toString(2));
+
+        Response response = apiHelper.postApiTest(ENDPOINT, requestJsonString, 201);
+        JSONObject responseJson = new JSONObject(response.getBody().asString());
+        System.out.println("POST Response: "+responseJson.toString(2));
+        assertDataType(requestJson, responseJson);
+
+    }
+
+    public void assertDataType(JSONObject jsonRequest, JSONObject jsonResponse) {
+
+        String expectedUserIdDataType = "Integer";
+        String expectedIdDataType = "Integer";
+        String expectedTitleDataType = "String";
+        String expectedBodyDataType = "String";
+
+        if (jsonRequest != null) {
+            expectedUserIdDataType = jsonRequest.get("userId").getClass().getSimpleName();
+            expectedTitleDataType = jsonRequest.get("title").getClass().getSimpleName();
+            expectedBodyDataType = jsonRequest.get("body").getClass().getSimpleName();
+        }
+
+        //assert userId data type
+        String userIdDataType = jsonResponse.get("userId").getClass().getSimpleName();
+        Assert.assertEquals(expectedUserIdDataType, userIdDataType);
+
+        //assert id data type
+        String idDataType = jsonResponse.get("id").getClass().getSimpleName();
+        Assert.assertEquals(expectedIdDataType, idDataType);
+
+        //assert title data type
+        String titleDataType = jsonResponse.get("title").getClass().getSimpleName();
+        Assert.assertEquals(expectedTitleDataType, titleDataType);
+
+        //assert body data type
+        String bodyDataType = jsonResponse.get("body").getClass().getSimpleName();
+        Assert.assertEquals(expectedBodyDataType, bodyDataType);
+    }
+}
